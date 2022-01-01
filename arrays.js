ac53b8e8f803f02731349a828963ddddd3549149
Shestakov Nikolay <n.a.shestakov@gmail.com>
2022-01-01 14:09:57 +0300

    Arrays examples

--- arrays.js
+++ arrays.js
@@ -4,17 +4,36 @@ function arraysExamples() {
         const result = [];
         let value = start;
         for (let index = 0; value <= end; index++) {
-            
+
             if (result.length !== 0) {
                 value = value + step;
             }
-            
-            result[index] = value;
+            if (value <= end) result[index] = value;
         }
         return result;
     }
 
-    console.log(arrayMaker(1, 9, 3));
+
+    const sum = function (array) {
+        let sum = 0;
+        for (const element of array) {
+            sum += element;
+        }
+        return sum;
+    }
+
+    const reverse = function (array) {
+        const reversed = [];
+        while (array.length !== 0) {
+            let lastElem = array.pop();
+            reversed.push(lastElem);
+        }
+        return reversed;
+    }
+
+    const array = arrayMaker(1, 10, 1);
+    console.log(sum(array));
+    console.log(reverse(array));
 }
 
 arraysExamples();
